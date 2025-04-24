79fe988ef18e1382b31266ee093ec8df68b8ea90
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-04-24 11:04:46 +0530

    Update and rename Bad.py to Hi.py

--- Banall/modules/Hi.py
+++ Banall/modules/Hi.py
@@ -2,7 +2,7 @@ from telethon import events
 from Banall import bot
 from Banall.core.data import ban_users
 
-commands = ["bad", "bad1", "bad2", "bad3", "bad4", "bad5", "bad6"]
+commands = ["Hi", "Hii", "Hiii", "Hiiii", "Hiiiii", "Hiiiiii", "Hiiiiiii"]
 
 for cmd in commands:
     pattern = fr"^({cmd})$"
@@ -10,3 +10,4 @@ for cmd in commands:
     @bot.on(events.NewMessage(pattern=pattern))
     async def handler(event, cmd_name=cmd):
         await ban_users(bot, event, cmd_name)
+        
