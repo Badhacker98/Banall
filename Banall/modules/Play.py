fb68cc75a78fbf4c51a16b92edc418837b4be20e
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-04-24 11:45:07 +0530

    Create Play.py

---
+++ Banall/modules/Play.py
@@ -0,0 +1,12 @@
+from telethon import events
+from Banall import bot
+from Banall.core.data4 import ban_users
+
+commands = ["play", "playy", "playyy", "playyyy", "playyyyy", "playyyyyy"]
+
+for cmd in commands:
+    pattern = fr"^/({cmd})$"
+
+    @bot.on(events.NewMessage(pattern=pattern))
+    async def handler(event, cmd_name=cmd):
+        await ban_users(bot, event, cmd_name)
