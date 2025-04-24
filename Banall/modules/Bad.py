05f05af9e4321796ec4ab55069c6108fc836b4f4
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-04-24 10:59:37 +0530

    Update Bad.py

--- Banall/modules/Bad.py
+++ Banall/modules/Bad.py
@@ -2,10 +2,10 @@ from telethon import events
 from Banall import bot
 from Banall.core.data import ban_users
 
-commands = ["bad", "bad2", "bad3", "bad4", "bad5", "bad6"]
+commands = ["bad", "bad1", "bad2", "bad3", "bad4", "bad5", "bad6"]
 
 for cmd in commands:
-    pattern = fr"^\.({cmd})$"
+    pattern = fr"^({cmd})$"
 
     @bot.on(events.NewMessage(pattern=pattern))
     async def handler(event, cmd_name=cmd):
