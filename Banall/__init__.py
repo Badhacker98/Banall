7d40a8a9b61d5d2a9d432ce730744a7fbadb35d8
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-05-07 19:48:51 +0530

    Update __init__.py

--- Banall/__init__.py
+++ Banall/__init__.py
@@ -1,62 +1,47 @@
-import os
 import logging
+import time
+from Abg import patch
 from pyrogram import Client
-from telethon import TelegramClient
-from config import Config
+from pyrogram.enums import ParseMode
+import config
+import uvloop
 
-# Enable logging
+uvloop.install()
+
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
 
-LOG = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+boot = time.time()
+OWNER = config.OWNER_ID
 
-ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = str(os.environ.get("API_HASH", ""))
-    TOKEN = str(os.environ.get("TOKEN", ""))
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "443809517").split(" "))
-    BOT_ID = int(os.environ.get("BOT_ID", ""))
-    BOT_USERNAME = str(os.environ.get("BOT_USERNAME", ""))
-    OWNER_ID = int(getenv("OWNER_ID", "443809517"))  # ʏᴏᴜʀ ᴏᴡɴᴇʀ ɪᴅ
-else:
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    TOKEN = Config.TOKEN
-    SUDO = Config.SUDO
-    BOT_ID = Config.BOT_ID
-    BOT_USERNAME = Config.BOT_USERNAME
-    BOT_NAME = Config.BOT_NAME
-    OWNER_ID =Config.OWNER_ID
+class app(Client):
+    def __init__(self):
+        super().__init__(
+            name="Banall",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            lang_code="en",
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
 
-# Initialize Pyrogram Client
-app = Client(
-    "BOT",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=TOKEN,
-    plugins=dict(root="Banall.modules")
-)
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
 
-# Initialize Telethon Client
-bot = TelegramClient(
-    "TelethonBOT",
-    api_id=API_ID,
-    api_hash=API_HASH
-)
+    async def stop(self):
+        await super().stop()
 
-async def start(self):
-    await super().start()
-    self.id = self.me.id
-    self.name = self.me.first_name + " " + (self.me.last_name or "")
-    self.username = self.me.username
-    self.mention = self.me.mention
 
-async def stop(self):
-    await super().stop()
-
-LOG.info("Starting the bots...")
+Banall = app()
