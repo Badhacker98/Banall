0422683e890306f72ad771a343fefc438ab6b8e4
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-05-08 08:18:17 +0530

    Update __init__.py

--- Banall/__init__.py
+++ Banall/__init__.py
@@ -36,8 +36,8 @@ app = Client(
 bot = TelegramClient(
     "TelethonBOT",
     api_id=API_ID,
-    api_hash=API_HASH
-)
+    api_hash=Config.HASH_ID
+).start(bot_token=BOT_TOKEN)
 
 async def start(self):
     await super().start()
